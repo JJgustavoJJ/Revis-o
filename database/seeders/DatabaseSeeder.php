fcce2a875c4eb506963be345fc36537a9053f258
JJgustavoJJ <gustavogaldino820@gmail.com>
2026-05-07 09:10:23 -0300

    iplementação seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Produto;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,33 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+       User::create([
+        'name' => 'Adimin',
+        'email' => 'admin@senai.br',
+        'passord' => Hash::make('123'),
+       ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+       Produto::create([
+        'nome' => 'Martelo',
+        'valor' => 50.90,
+        'qtn_estoque' => 0,
+        'qtn_minima' =>5
+       ]);
+
+       Produto::create([
+        'nome' => 'Alicate',
+        'valor' => 30,
+        'qtn_estoque' => 0,
+        'qtn_minima' => 3
+       ]);
+
+       Produto::create([
+        'nome' => 'Furadeira',
+        'valor' => 99.99,
+        'qtn_estoque' => 0,
+        'qtn_minima' => 8
+       ]);
+        
+        
     }
 }
